9d751f9408d2a2725585f5e59e21c5276534a1cb
Pierre Laferriere <polaferriere@gmail.com>
2015-09-09 15:43:30 -0400

    Finished project.  Got the javascript to run well and show what needed to be shown.  No further changes should be needed.

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+
+function leadingZero (numb) {
+  var number = String(numb);
+  while (number.length < 2) {
+    number = "0" + number;
+  }
+  return number;
+}
+
+var time = new Date();
+var hours = leadingZero(time.getHours());
+var minutes = leadingZero(time.getHours());
+var seconds = leadingZero(time.getSeconds());
+var currentTime = hours + ":" + minutes + ":" + seconds;
+document.querySelector(".time").textContent = currentTime;
+
+var hexColor = leadingZero(Number(hours).toString(16)) + leadingZero(Number(minutes).toString(16)) + leadingZero(Number(seconds).toString(16));
+
+var background = document.querySelector('body');
+background.style.background = '#' + hexColor;
+
+var barWidth = Number(Math.round((time.getSeconds() / 60) * 100));
+
+var bar = document.querySelector('hr');
+bar.style.width = barWidth + '%';
+
+
+
+function timeRefresh() {
+  time = new Date();
+
+  hours = leadingZero(time.getHours());
+  minutes = leadingZero(time.getHours());
+  seconds = leadingZero(time.getSeconds());
+  currentTime = hours + ":" + minutes + ":" + seconds;
+
+
+
+  hexColor = leadingZero(Number(hours).toString(16)) + leadingZero(Number(minutes).toString(16)) + leadingZero(Number(seconds).toString(16));
+
+  document.querySelector(".time").textContent = currentTime;
+
+  document.querySelector(".hex").textContent = "#" + hexColor;
+
+  barWidth = Number(Math.round((time.getSeconds() / 60) * 100));
+
+  bar.style.width = barWidth + '%';
+
+  background.style.background = '#' + hexColor;
+}
+
+setInterval(timeRefresh, 1000);
